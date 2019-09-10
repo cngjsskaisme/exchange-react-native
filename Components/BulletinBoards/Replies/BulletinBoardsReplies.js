06c7d6ab08c4752949d0ae5fa77c51a67d670198
flyc4 <37125974+flyc4@users.noreply.github.com>
2019-09-10 22:33:03 +0900

    댓글 불러오기 및 추가 완료

--- Components/BulletinBoards/Replies/BulletinBoardsReplies.js
+++ Components/BulletinBoards/Replies/BulletinBoardsReplies.js
@@ -8,10 +8,13 @@
 
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import PropTypes from 'prop-types';
-import { CommentEntries_Mock }  from '../../../Mockup_Datas/UnifiedEntries'
-import BulletinBoardsRepliesEntries from './BulletinBoardsRepliesEntries'
+import { CommentEntries_Mock }  from '../../../Mockup_Datas/UnifiedEntries'; 
+import BulletinBoardsRepliesEntries from './BulletinBoardsRepliesEntries'; 
+import axios from 'axios'; 
+import {server} from '../../ServerLib/config';
+
 
 class BulletinBoardsReplies extends Component{
     static defaultProps = {
@@ -19,7 +22,10 @@ class BulletinBoardsReplies extends Component{
         replyid: 0,
         userid: 0,
         username: '',
-        profile: ''
+        profile: '', 
+        //가져올 댓글의 시작, 끝 인덱스 번호 
+        commentstartindex: 0, 
+        commentendindex: 19,
     }
 
     constructor(props){
@@ -29,9 +35,48 @@ class BulletinBoardsReplies extends Component{
             replyid: this.props.replyid,
             userid: this.props.userid,
             username: this.props.username,
-            profile: this.props.profile
+            profile: this.props.profile,
+            //가져올 댓글의 시작, 끝 인덱스 번호 
+            commentstartindex: this.props.commentstartindex, 
+            commentendindex: this.props.commentendindex,             
         }
     }
+
+    //데이터 받아오기 시작 
+    _onGetComments = async () => {   
+        var url = server.serverURL + '/process/ShowComments';
+        this.setState({
+            isLoading: true,
+            isError: false
+        }) 
+        await axios.post(url, {userid: "5d5373177443381df03f3040", boardid: "board1", 
+            entryid: "5d75a757d47cdf78a5ce79d1", 
+            commentstartindex: this.state.commentstartindex, commentendindex: this.state.commentendindex}) 
+
+            .then((response) => {       
+                this.setState({ 
+                commentslist: response.data.commentslist,
+                isLoading: false
+                }) 
+            }) 
+            .catch(( err ) => {
+                Alert.alert(
+                    'Cannot connect to the server. Falling back to default option.',
+                    'There are two possible errors : \n 1. Your Phone is not connected to the internet. \n 2. The server is not available right now.',
+                    [{text: 'OK'}]
+                );
+            this.setState({
+                commentslist: CommentEntries_Mock,
+                isError: true
+            })
+        });    
+    }
+    async componentDidMount(){
+      await this. _onGetComments(); 
+    }
+    //데이터 받아오기 끝 
+
+
 
     _renderItem = ({ item }) => {
         return(
@@ -55,7 +100,7 @@ class BulletinBoardsReplies extends Component{
     render(){
         return(
             <FlatList 
-                data = {CommentEntries_Mock} // !!!!<-- 창희야 여기에 json 댓글을 반환해야 돼!!!! //
+                data = {this.state.commentslist} 
                 renderItem = {this._renderItem}
                 keyExtractor = {this._keyExtractor}/>
         );
