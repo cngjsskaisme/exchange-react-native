856d4c980588ff64f3d3086f500fa10575721133
flyc4 <37125974+flyc4@users.noreply.github.com>
2019-09-21 21:34:42 +0900

    공지사항 번역 (영어, 중국어) 완료

--- Components/BulletinBoards/BulletinBoardsLists.js
+++ Components/BulletinBoards/BulletinBoardsLists.js
@@ -39,7 +39,7 @@ class BulletinBoardsLists extends Component{
             currentuserid: "5d5373177443381df03f3040", //userid 반환 여기서 하게끔 boardslist에서 불러오기 (onget)
             isLoading: false,
             isError: false,
-            isDev: true //개발자 모드는 여기서 활성화
+            isDev: false //개발자 모드는 여기서 활성화
         }
     }
 
