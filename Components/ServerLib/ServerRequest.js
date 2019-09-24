04592e28dd442fccd4ff5f20953a86c82d76d857
flyc4 <37125974+flyc4@users.noreply.github.com>
2019-09-24 17:56:20 +0900

    ServerRequest 함수 13개 반영 완료

--- Components/ServerLib/ServerRequest.js
+++ Components/ServerLib/ServerRequest.js
@@ -1,15 +1,21 @@
+import _handleAddReport from './Requests/_handleAddReport'
+import _handleBulletinBoardsPostDelete from './Requests/_handleBulletinBoardsPostDelete'
+import _handleBulletinBoardsPostSubmit from './Requests/_handleBulletinBoardsPostSubmit'
+import _handleDeleteDM from './Requests/_handleDeleteDM'
+import _handleDeleteReplies from './Requests/_handleDeleteReplies' 
+import _handleGetDMList from './Requests/_handleGetDMList'
+import _handleGetUserNameList from './Requests/_handleGetUserNameList'
+import _handleLikeIncrease from './Requests/_handleLikeIncrease'
 import _onGetBulletinBoardsLists from './Requests/_onGetBulletinBoardsLists'
 import _onGetBulletinBoardsPost from './Requests/_onGetBulletinBoardsPost'
 import _onGetBulletinBoardsReplies from './Requests/_onGetBulletinBoardsReplies'
-import _handleBulletinBoardsPostSubmit from './Requests/_handleBulletinBoardsPostSubmit'
-import _handleBulletinBoardsPostDelete from './Requests/_handleBulletinBoardsPostDelete'
-import _handleLikeIncrease from './Requests/_handleLikeIncrease'
-import _handleAddReport from './Requests/_handleAddReport'
-import _handleDeleteReplies from './Requests/_handleDeleteReplies'
-import _onAddBulletinBoardsReplies from './Requests/_onAddBulletinBoardsReplies'
+import _onAddBulletinBoardsReplies from './Requests/_onAddBulletinBoardsReplies' 
+import _handleSendDM from './Requests/_handleSendDM'
 
-export { _onGetBulletinBoardsLists, _onGetBulletinBoardsPost, _onGetBulletinBoardsReplies, _handleBulletinBoardsPostSubmit,
-    _handleLikeIncrease, _handleAddReport, _handleDeleteReplies, _handleBulletinBoardsPostDelete, _onAddBulletinBoardsReplies}
+
+export {_handleAddReport, _handleBulletinBoardsPostDelete, _handleBulletinBoardsPostSubmit, _handleDeleteDM, _handleDeleteReplies,
+    _handleGetDMList, _handleGetUserNameList, _handleLikeIncrease, _onGetBulletinBoardsLists, _onGetBulletinBoardsPost, _onGetBulletinBoardsReplies,
+    _onAddBulletinBoardsReplies, _handleSendDM}
 
 /*
 작성자: 추헌남
