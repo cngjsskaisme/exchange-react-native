9986323fe7d64cc7be328b976d841a2b3d2fa090
flyc4 <37125974+flyc4@users.noreply.github.com>
2019-09-28 12:18:53 +0900

    ServerLib/Request 정리 및 EventCalendar 일부 구현

--- Components/ServerLib/ServerRequest.js
+++ Components/ServerLib/ServerRequest.js
@@ -1,21 +1,39 @@
-import _handleAddReport from './Requests/_handleAddReport'
-import _handleBulletinBoardsPostDelete from './Requests/_handleBulletinBoardsPostDelete'
-import _handleBulletinBoardsPostSubmit from './Requests/_handleBulletinBoardsPostSubmit'
-import _handleDeleteDM from './Requests/_handleDeleteDM'
-import _handleDeleteReplies from './Requests/_handleDeleteReplies' 
-import _handleGetDMList from './Requests/_handleGetDMList'
-import _handleGetUserNameList from './Requests/_handleGetUserNameList'
-import _handleLikeIncrease from './Requests/_handleLikeIncrease'
-import _onGetBulletinBoardsLists from './Requests/_onGetBulletinBoardsLists'
-import _onGetBulletinBoardsPost from './Requests/_onGetBulletinBoardsPost'
-import _onGetBulletinBoardsReplies from './Requests/_onGetBulletinBoardsReplies'
-import _onAddBulletinBoardsReplies from './Requests/_onAddBulletinBoardsReplies' 
-import _handleSendDM from './Requests/_handleSendDM'
+//BulletinBoard 관련
+import _handleAddReport from './Requests/BulletinBoards/_handleAddReport'
+import _handleBulletinBoardsPostDelete from './Requests/BulletinBoards/_handleBulletinBoardsPostDelete'
+import _handleBulletinBoardsPostSubmit from './Requests/BulletinBoards/_handleBulletinBoardsPostSubmit'
+import _handleDeleteDM from './Requests/BulletinBoards/_handleDeleteDM'
+import _handleDeleteReplies from './Requests/BulletinBoards/_handleDeleteReplies' 
+import _handleGetDMList from './Requests/BulletinBoards/_handleGetDMList'
+import _handleGetUserNameList from './Requests/BulletinBoards/_handleGetUserNameList'
+import _handleLikeIncrease from './Requests/BulletinBoards/_handleLikeIncrease'
+import _onGetBulletinBoardsLists from './Requests/BulletinBoards/_onGetBulletinBoardsLists'
+import _onGetBulletinBoardsPost from './Requests/BulletinBoards/_onGetBulletinBoardsPost'
+import _onGetBulletinBoardsReplies from './Requests/BulletinBoards/_onGetBulletinBoardsReplies'
+import _onAddBulletinBoardsReplies from './Requests/BulletinBoards/_onAddBulletinBoardsReplies' 
+import _handleSendDM from './Requests/BulletinBoards/_handleSendDM'
+
+//EventCalendar 관련  
+import _handleDeleteEvent from './Requests/EventCalendar/_handleDeleteEvent'
+import _handleEditEvent from './Requests/EventCalendar/_handleEditEvent'
+import _handleGetEvents from './Requests/EventCalendar/_handleGetEvents'
 
 
-export {_handleAddReport, _handleBulletinBoardsPostDelete, _handleBulletinBoardsPostSubmit, _handleDeleteDM, _handleDeleteReplies,
+
+
+export {
+    
+    //BulletinBoard 관련
+    _handleAddReport, _handleBulletinBoardsPostDelete, _handleBulletinBoardsPostSubmit, _handleDeleteDM, _handleDeleteReplies,
     _handleGetDMList, _handleGetUserNameList, _handleLikeIncrease, _onGetBulletinBoardsLists, _onGetBulletinBoardsPost, _onGetBulletinBoardsReplies,
-    _onAddBulletinBoardsReplies, _handleSendDM}
+    _onAddBulletinBoardsReplies, _handleSendDM, 
+
+    //EventCalendar 관련
+    _handleDeleteEvent, _handleEditEvent, _handleGetEvents, 
+
+
+
+}
 
 /*
 작성자: 추헌남
