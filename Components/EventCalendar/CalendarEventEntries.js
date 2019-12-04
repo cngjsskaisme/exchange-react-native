da57cc8a6d15598a46faa41f33457c398f8075d7
Chang Hee Kim <flyc4@naver.com>
2019-12-04 21:10:30 +0900

    event calendar mockup 추가

--- Components/EventCalendar/CalendarEventEntries.js
+++ Components/EventCalendar/CalendarEventEntries.js
@@ -1,7 +1,87 @@
 // EventEntries[1].Date
 export const EventEntries = [
     {
-        Date : '2019-10-04',
+        Date : '2019-12-01',
+        Events : [
+            {
+                isAdmin : true,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '3:00PM - 5:00PM',
+                title : 'Club activity Day', 
+                type : [''],
+                contents : 'Some event',
+                eventID : '01',                
+            }, 
+
+            {
+                isAdmin : true,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '4:00PM - 6:00PM',
+                title : 'First day of intern conference day', 
+                type : [''],
+                contents : 'Intern Recruit',
+                eventID : '02',                
+            },
+        ],  
+    },
+    {
+        Date : '2019-12-02',
+        Events : [
+            {
+                isAdmin : true,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '3:00PM - 5:00PM',
+                title : 'First day of vote', 
+                type : [''],
+                contents : 'Vote',
+                eventID : '03',                
+            }, 
+
+            {
+                isAdmin : true,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '4:00PM - 6:00PM',
+                title : 'Second day of intern conference day', 
+                type : [''],
+                contents : 'conference',
+                eventID : '04',                
+            },
+        ],  
+    }, 
+    
+    {
+        Date : '2019-12-03',
+        Events : [
+            {
+                isAdmin : true,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '2:00PM - 6:00PM',
+                title : 'Second day of vote', 
+                type : [''],
+                contents : 'Vote',
+                eventID : '05',                
+            }, 
+
+            {
+                isAdmin : true,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '4:00PM - 6:00PM',
+                title : 'Last day of intern conference day', 
+                type : [''],
+                contents : 'Intern',
+                eventID : '06',                
+            },
+        ],  
+    }, 
+    {
+
+        Date : '2019-12-04',
         Events : [
             {
                 isAdmin : true,
@@ -11,8 +91,7 @@ export const EventEntries = [
                 title : 'Teacher Day', 
                 type : [''],
                 contents : 'Babababababa .... ahdjasd',
-                eventID : '12',
-                
+                eventID : '12',                
             },
             {
                 isAdmin : false,
@@ -103,7 +182,7 @@ export const EventEntries = [
     },
 
     {
-        Date : '2019-10-05',
+        Date : '2019-12-05',
         Events : [
             {
                 isAdmin : false,
@@ -155,10 +234,122 @@ export const EventEntries = [
             },
             
         ]
-    },
+    }, 
+    
+    {
+        Date : '2019-12-06',
+        Events : [
+            {
+                isAdmin : false,
+                distributor : 'Course Review',
+                place : 'Hanyang University',
+                time : '4:00PM - 10:00PM',
+                title : 'Course', 
+                typed : [''],
+                contents : 'asdaf edsfade sae fa....',
+
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '11:00AM - 01:00PM',
+                title : 'Haksul Town Review', 
+                typed : [''],
+                contents : 'asdaf edsfade sae fa....',
+
+                eventID : '12',
+                
+            }, 
+        ] 
+    }, 
+    {
+        Date: '2019-12-07', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'HISA',
+                place : 'Hanyang University',
+                time : '4:00PM - 6:30PM',
+                title : 'HISA Recruit', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'PBL',
+                place : 'Hanyang University',
+                time : '1:00PM - 10:00PM',
+                title : 'Haksul Town conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+    
+    {
+        Date: '2019-12-08', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Engineering',
+                place : 'Hanyang University',
+                time : '2:30PM - 6:00PM',
+                title : 'engineering Conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '3:00PM - 6:00PM',
+                title : 'Job Conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
 
     {
-        Date : '2019-10-10',
+        Date: '2019-12-09', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                time : '4:30PM - 7:00PM',
+                title : 'Student Conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '6:00PM - 10:00PM',
+                title : 'SKT Conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    },
+    {
+        Date : '2019-12-10',
         Events : [
             {
                 isAdmin : false,
@@ -210,7 +401,280 @@ export const EventEntries = [
             },
             
         ]
+    },  
+
+    {
+        Date: '2019-12-11', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Leadership Center',
+                time : '5:30PM - 7:00PM',
+                title : 'Leadership Education', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'HISA',
+                place : 'Hanyang University',
+                time : '6:00PM - 8:00PM',
+                title : 'HISA Recruitment', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+
+    {
+        Date: '2019-12-12', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Leadership Center',
+                time : '4:00PM - 6:00PM',
+                title : 'HELP1 Conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Engineering Center',
+                place : 'Hanyang University',
+                time : '7:00PM - 9:00PM',
+                title : 'HYUNDAI Card recruitment conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+
+    {
+        Date: '2019-12-13', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                time : '2:00PM - 4:30PM',
+                title : 'Winter School Registration', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Leadership Center',
+                place : 'Hanyang University',
+                time : '4:30PM - 6:30PM',
+                title : 'HY-WEP Recuitment', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+
+    {
+        Date: '2019-12-14', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                time : '1:00PM - 3:30PM',
+                title : 'Welcoming Exchange Students', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Engineering Center',
+                place : 'Hanyang University',
+                time : '5:30PM - 7:30PM',
+                title : 'Programming Contest', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+
+    {
+        Date: '2019-12-15', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                time : '6:00PM - 10:00PM',
+                title : 'Home Coming party', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'PAE Center',
+                place : 'Hanyang University',
+                time : '5:30PM - 7:30PM',
+                title : 'PAE Register', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+
+    {
+        Date: '2019-12-16', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                time : '8:00PM - 10:00PM',
+                title : 'Graduate Party', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                place : 'Hanyang University',
+                time : '1:00PM - 3:00PM',
+                title : 'Free lunch for students', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
     },
+
+    {
+        Date: '2019-12-17', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University',
+                time : '2:00PM - 3:00PM',
+                title : 'Suwon Cultural trip', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'LeaderShip Center',
+                place : 'Hanyang University',
+                time : '1:00PM - 3:00PM',
+                title : 'HELP2 Conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    },
+
+    {
+        Date: '2019-12-18', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Engineering Center',
+                time : '12:00PM - 2:00PM',
+                title : 'Samsung SDS conference', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'LeaderShip Center',
+                place : 'Hanyang University',
+                time : '1:00PM - 3:00PM',
+                title : 'Education on writing a resume', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+
+    {
+        Date: '2019-12-19', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Paiknam Library',
+                time : '3:00PM - 7:00PM',
+                title : 'Reading Quiz Contest', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'international Center',
+                place : 'Hanyang University',
+                time : '4:00PM - 6:00PM',
+                title : 'Gongja Academy', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    }, 
+
+    {
+        Date: '2019-12-20', 
+        Events: [
+            {
+                isAdmin : false,
+                distributor : 'Hanyang University Church',
+                time : '5:00PM - 7:00PM',
+                title : 'Christmas giving event', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+                
+            },
+            {
+                isAdmin : false,
+                distributor : 'Hanyang Center',
+                place : 'Hanyang University',
+                time : '4:00PM - 6:00PM',
+                title : 'Year End Party', 
+                typed : [''],
+                contents : 'Babababababa .... ahdjasd',
+                eventID : '12',
+            },
+            
+        ]
+    },
+
+
+
 ];
 
 export const EventInfo = [
